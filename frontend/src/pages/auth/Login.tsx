8fc48dccccf9c2f8b2ab7fbb352e27b1d928bb59
Madhu Yadhav <madhuyadhav118@gmail.com>
2026-08-01 19:00:56 +0530

    Day 38: Frontend setup and first React login component

---
+++ frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,28 @@
+function Login() {
+  return (
+    <div>
+      <h1>College AI Assistant</h1>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Enter Email"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Enter Password"
+      />
+
+      <br />
+      <br />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
